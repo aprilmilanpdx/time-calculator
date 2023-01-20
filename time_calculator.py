806cd2491a89df1734ee9c7c1d4baeb692e1440a
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-01-20 11:26:44 -0800

    added calculators, known bug to create correct midnight output

--- time_calculator.py
+++ time_calculator.py
@@ -1,10 +1,52 @@
-def add_time(start, duration):
-  s_time = start.split()[0]
+def add_time(start, duration, day = "Today"):
+  time = start.split()[0]
   clock= start.split()[1]
-  print(s_time)
-  print(clock)
+  s_hour = int(time.split(':')[0])
+  s_minute = int(time.split(':')[1])
+  d_hour = int(duration.split(':')[0])
+  d_minute = int(duration.split(':')[1])
+  
+  # print(time)
+  # print(clock)
+  # print(s_hour)
+  # print(s_minute)
+  # print(d_hour)
+  # print(d_minute)
 
+  if clock == "PM":
+    s_hour += 12
+  # print(s_hour)
 
+  new_hour = s_hour + d_hour
+  # print("hour", new_hour)
 
+  new_minute = s_minute + d_minute
+  if new_minute > 59:
+    new_minute -= 60
+    new_hour += 1
+  # print("minute", new_minute)
+  # print("hour", new_hour)
 
-add_time("11:06 PM", "2:02")
+  new_day = 0
+  if new_hour > 23:
+    new_hour -= 24
+    # bug in this code?  need to get midnight to list as 12:00 AM
+    new_day += 1
+
+  print("hour", new_hour)
+  # print("day", new_day)
+  
+  new_clock = ""
+  if new_hour > 0 and new_hour < 12:
+    new_clock = "AM"
+  else:
+    new_clock = "PM"
+  
+  if new_hour > 12:
+    new_hour -= 12
+  
+
+  print("new clock", new_clock)
+  print("new hour", new_hour)
+
+add_time("11:06 PM", "0:54")
